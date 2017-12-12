3aa41c31f052f629bd9f1d79f521fd869251561c
Zachary Rust <zrust001@student.butte.edu>
2017-12-12 10:22:17 +0000

    5.0assignment

--- Assignments/5.0assignment/main.cpp
+++ Assignments/5.0assignment/main.cpp
@@ -1,3 +1,8 @@
+//Zachary Rust
+//12/12/17
+//Assignment 5.0
+//This is a register for simple companies with limited
+//items on their menu.  It prints recipts to a fiel.
 #include "manager.h"
 using namespace std;
 
